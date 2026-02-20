e7453d1e761610294a0779f9404e7702e767cf4d
xsararex <90262746+xsararex@users.noreply.github.com>
2026-02-20 15:06:35 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,35 @@
 const CACHE_NAME = 'img2pdf-cache-v1';
+
 const urlsToCache = [
+  './',
   './index.html',
   './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
